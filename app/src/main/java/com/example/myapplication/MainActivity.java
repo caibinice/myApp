00e79038c57b894acd55dd472a043b44784f735f
fish <391459189@qq.com>
2018-10-03 19:13:08 +0900

    build project,add QR code demo
    新建android studio项目，依照官方文档写hello world程序，加入第三方开源的二维码验证功能页面。

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private EditText mNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNumber = (EditText) findViewById(R.id.editText);
+        Button mCallButton = (Button) findViewById(R.id.button);
+        mCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获得输入框中的号码
+                String number = mNumber.getText().toString().trim();
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + number));
+                startActivity(intent);
+            }
+        });
+    }
+
+    /** Called when the user taps the Send button */
+    public void nextPage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the Start button */
+    public void toQRcode(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, QRcodeActivity.class);
+        startActivity(intent);
+    }
+}
